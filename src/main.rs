c1e8881fd7acdbace9b1efcab61d7a225e9cfdb9
RubixDev <groh@rubixdev.de>
2026-06-02 18:16:06 +0200

    feat: also ignore "an" prefix in sort

--- src/main.rs
+++ src/main.rs
@@ -143,6 +143,7 @@ fn main() -> Result<()> {
     index.sort_unstable_by_key(|w| {
         let game_sort = w.display_name.as_ref().unwrap_or(&w.game).to_lowercase();
         let game_sort = game_sort.strip_prefix("the ").unwrap_or(&game_sort);
+        let game_sort = game_sort.strip_prefix("an ").unwrap_or(game_sort);
         let game_sort = game_sort.strip_prefix("a ").unwrap_or(game_sort);
         game_sort.to_owned()
     });
